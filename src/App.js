7e7670cfc3bfc582f7a4dda1285033a891d43f8b
maaliwageesha <malidi26@yahoo.com>
2024-10-05 14:31:08 +0530

    added route to dashboard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { Dashboard } from './pages/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactks
-        </a>
-      </header>
+       <BrowserRouter>
+       <Routes>
+       <Route path="/" element={<Dashboard />} />
+
+
+       </Routes>
+       </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+
+import './Dashboard.scss';
+export const Dashboard = ()=>{
+
+    return(
+        <div>
+            this is dashboard
+        </div>
+    )
+}
