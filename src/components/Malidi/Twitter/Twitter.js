f5d2d3bcdd50a2026407a706e5afdd88f8621dfd
maaliwageesha <malidi26@yahoo.com>
2024-10-13 00:45:49 +0530

    updated all twitter graphs

--- src/components/Malidi/Twitter/Twitter.js
+++ src/components/Malidi/Twitter/Twitter.js
@@ -11,6 +11,24 @@ export const Twitter =(selectedCountry)=>{
       style={{ width: '100%', height: '500px', border: 'none' }}
     />
         </div>}
+
+
+        {selectedCountry.selectedCountry=="singapore" && <div>
+          <iframe
+      src="twitter/singapore.html"
+      title="Plotly Graph"
+      style={{ width: '100%', height: '500px', border: 'none' }}
+    />
+        </div>}
+
+
+        {selectedCountry.selectedCountry=="india" && <div>
+          <iframe
+      src="twitter/india.html"
+      title="Plotly Graph"
+      style={{ width: '100%', height: '500px', border: 'none' }}
+    />
+        </div>}
                     {selectedCountry.selectedCountry=="china" && <div>
           <iframe
       src="twitter/china.html"
