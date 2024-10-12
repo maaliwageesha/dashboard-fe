673fd59bbc397818cd4df3bae0d40fcb5ef8e6f8
Bhumi <gorasvabhumi@gmail.com>
2024-10-12 17:11:56 +1100

    Intial commit

---
+++ src/components/Bhumi/Analysis.js
@@ -0,0 +1,19 @@
+export const Analysis = ({ selectedCountry }) => {
+    return (
+        <div>
+            {selectedCountry === "australia" && (
+                <iframe
+                    src="Bhumi/Australia_estimated_subsidies_renewable_energy.html"
+                    title="Plotly Graph - Australia"
+                    style={{ width: '100%', height: '500px', border: 'none' }}
+                />
+            )}
+            {/* Optional: Handling case when no country is selected */}
+            {selectedCountry && !["australia", "india", "china", "singapore"].includes(selectedCountry) && (
+                <div>
+                    <p>Please select a valid country to view the renewable energy graph.</p>
+                </div>
+            )}
+        </div>
+    );
+}
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,102 +1,99 @@
-
-import React ,{ useState } from 'react';
-import {ElectricityUsage} from '../../components/Malidi/Consumption/ElectricityUsage';
-import './Dashboard.scss';
+import React, { useState } from 'react';
+import { ElectricityUsage } from '../../components/Malidi/Consumption/ElectricityUsage';
 import { Twitter } from '../../components/Malidi/Twitter/Twitter';
 import { Reddit } from '../../components/Malidi/Reddit/Reddit';
 import { Usage } from '../../components/Dilanthi/Usage/Usage';
-export const Dashboard = ()=>{
-    const [selectedCountry, setSelectedCountry] = useState('australia');
+import './Dashboard.scss';
+import { Analysis } from '../../components/Bhumi/Analysis';
 
-    const handleCountryChange = (event) => {
-        setSelectedCountry(event.target.value);
-    };
+// A reusable component for country selection dropdown
+const CountrySelect = ({ selectedCountry, onChange }) => (
+  <select value={selectedCountry} onChange={onChange}>
+    <option value="australia">Australia</option>
+    <option value="india">India</option>
+    <option value="singapore">Singapore</option>
+    <option value="china">China</option>
+  </select>
+);
 
-    
+export const Dashboard = () => {
+  const [selectedCountry, setSelectedCountry] = useState('australia');
 
-    return(
-        <div className='border-margin'>
-            <div className='padding-top'>
-            <div className='heading'>
-                <h1 className='heading-txt'>DASHBOARD FOR RENEWABLE ENERGY</h1>
-            </div>
+  // Handle country selection changes
+  const handleCountryChange = (event) => {
+    setSelectedCountry(event.target.value);
+  };
 
-            <div>
-                    <select value={selectedCountry} onChange={handleCountryChange}>
-                    <option value="australia">Australia</option>
-                    <option value="india">India</option>
-                    <option value="singapore">Singapore</option>
-                    <option value="china">China</option>
-                    </select>
-                </div>
+  return (
+    <div className='border-margin'>
+      <div className='padding-top'>
+        <div className='heading'>
+          <h1 className='heading-txt'>DASHBOARD FOR RENEWABLE ENERGY</h1>
+        </div>
 
-            <div className='heading2'>
-                <h1 className='heading-txt2'>Latest trends & current affairs on renewable energy</h1>
-            </div>
+        {/* Country Selection Dropdown */}
+        <CountrySelect selectedCountry={selectedCountry} onChange={handleCountryChange} />
 
-<div className='align-flex'>
-    <div style={{width:'50%'}}>
-            <Twitter selectedCountry={selectedCountry}/>
-            </div>
-            <div style={{width:'50%'}}>
-            <Reddit/>
-            </div>
-            </div>
-            <div className='heading2'>
-                <h1 className='heading-txt2'>Sentiment Trends with Prediction (2021-2026)</h1>
-            </div>
+        <div className='heading2'>
+          <h1 className='heading-txt2'>Latest trends & current affairs on renewable energy</h1>
+        </div>
 
-            <div>
-                <div>
-                    <select value={selectedCountry} onChange={handleCountryChange}>
-                    <option value="australia">Australia</option>
-                    <option value="india">India</option>
-                    <option value="singapore">Singapore</option>
-                    <option value="china">China</option>
-                    </select>
-                </div>
+        {/* Twitter and Reddit Components */}
+        <div className='align-flex'>
+          <div style={{ width: '50%' }}>
+            <Twitter selectedCountry={selectedCountry} />
+          </div>
+          <div style={{ width: '50%' }}>
+            <Reddit />
+          </div>
+        </div>
 
-                <div  style={{ marginTop: '20px' }}>
-                <iframe
-                    src={`/SAGraphs/${selectedCountry}_sentiment_trends.html`}
-                    width="100%"
-                    height="600px"
-                    title="Sentiment Trends"
-                    frameBorder="0"
-                ></iframe>
-                </div>
-            </div>
+        <div className='heading2'>
+          <h1 className='heading-txt2'>Sentiment Trends with Prediction (2021-2026)</h1>
+        </div>
 
-            {/* <iframe
-                    src="http://127.0.0.1:8051/"
-                    style={{ width: '100%', height: '600px', border: 'none', paddingLeft: '10px'}}
-                    title="Australia Sentiment"
-            /> */}
-            <div className='heading2'>
-                <h1 className='heading-txt2'>Social Network of Keywords in Renewable Energy Comments</h1>
-            </div>
-                
-                <iframe
-                    src="http://127.0.0.1:8053/"
-                    style={{ width: '100%', height: '600px', border: 'none', paddingLeft: '10px', marginTop: '20px' }}
-                    title="Australia Sentiment"/> 
+        {/* Country Select for Sentiment Trends */}
+        <CountrySelect selectedCountry={selectedCountry} onChange={handleCountryChange} />
 
-
-            <div className='heading2'>
-                <h1 className='heading-txt2'>Current & Future renewable energy production analysis</h1>
-            </div>
-                <ElectricityUsage selectedCountry={selectedCountry}/>
+        {/* Sentiment Trends iFrame */}
+        <div style={{ marginTop: '20px' }}>
+          <iframe
+            src={`/SAGraphs/${selectedCountry}_sentiment_trends.html`}
+            width="100%"
+            height="600px"
+            title="Sentiment Trends"
+            frameBorder="0"
+          ></iframe>
         </div>
         <div className='heading2'>
-                <h1 className='heading-txt2'>Usage of Renewable Energy </h1>
-            </div>
-            <Usage selectedCountry={selectedCountry}/>
+          <h1 className='heading-txt2'>Social Network of Keywords in Renewable Energy Comments</h1>
         </div>
 
+        {/* Social Network iFrame */}
+        <iframe
+          src="http://127.0.0.1:8053/"
+          style={{ width: '100%', height: '600px', border: 'none', paddingLeft: '10px', marginTop: '20px' }}
+          title="Social Network of Keywords"
+        ></iframe>
 
-        
+        <div className='heading2'>
+          <h1 className='heading-txt2'>Current & Future Renewable Energy Production Analysis</h1>
+        </div>
+        {/* Electricity Usage Component */}
+        <ElectricityUsage selectedCountry={selectedCountry} />
 
-        
-    )
-}
+        <div className='heading2'>
+          <h1 className='heading-txt2'>Usage of Renewable Energy</h1>
+        </div>
+        {/* Renewable Energy Usage Component */}
+        <Usage selectedCountry={selectedCountry} />
 
+        <div className='heading2'>
+          <h1 className='heading-txt2'>Annual Subsidies for Renewable Energy Projects Australia (2014-2023)</h1>
+        </div>
+        {/* Renewable Energy Usage Component */}
+        <Analysis selectedCountry={selectedCountry} />
+      </div>
+    </div>
+  );
+};
