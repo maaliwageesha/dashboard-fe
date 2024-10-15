88140277599568f81b78284fc61a64192d497d39
Shenuki Perera <shenuki101@gmail.com>
2024-10-15 19:34:27 +1100

    Dashboard layout edit
    Editing dashboard layout

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -5,10 +5,10 @@ export const Main = () =>{
     return(
         <div className="p-3">
 
-            <div className="text-start">Main Dashboard</div>
+            <div className="text-start">Main Dashboard</div>          
 
 
-            <div className="row">
+            {/* <div className="row">
                 <div className="col-4 p-3">
                     <div className="bg-white w-100 dash-cube">hi</div>
                 </div>
@@ -21,8 +21,75 @@ export const Main = () =>{
                     <div className="bg-white w-100 dash-cube">hi</div>
                 </div>
             
+            </div> */}
+
+            {/* Investment Graphs */}
+
+            <div className="row">
+                <div >
+                    <iframe
+                        src="Bhumi/Australia_estimated_subsidies_renewable_energy.html"
+                        title="Plotly Graph"
+                        style={{ width: '100%', height: '400px', border: 'none' }}
+                    />
+                </div>
             </div>
 
+            <div className="row">
+                <div className="col-6">
+                    <iframe
+                        src="Bhumi/sector_wise_subsidies_chart.html"
+                        title="Plotly Graph"
+                        style={{ width: '100%', height: '400px', border: 'none' }}
+                    />
+                </div>
+                <div className="col-6">
+                    <h4>Pie chart</h4>
+                    <iframe
+                        src=""
+                        title="Plotly Graph"
+                        style={{ width: '100%', height: '400px', border: 'none' }}
+                    />
+                </div>
+
+            </div>
+
+            {/* Consumption Graphs */}
+            <div className="row">
+                <div >
+                    <iframe
+                        src="Dil/Australia_renewable_energy.html"
+                        title="Plotly Graph"
+                        style={{ width: '100%', height: '400px', border: 'none' }}
+                    />
+                </div>
+            </div>
+
+            <div className="row">
+                <h4>Investment line graph</h4>
+                <iframe
+                    src=""
+                    title="Plotly Graph"
+                    style={{ width: '100%', height: '400px', border: 'none' }}
+                />
+            </div>
+            <div className="row">
+                    <iframe
+                        src="consumption/australia/CombineAll.html"
+                        title="Plotly Graph"
+                        style={{ width: '100%', height: '400px', border: 'none' }}
+                    />
+            </div>
+
+            {/* Sentiment Graphs */}
+
+            <div className="row">
+                    <iframe
+                        src="SAGraphs/australia_sentiment_trends.html"
+                        title="Plotly Graph"
+                        style={{ width: '100%', height: '400px', border: 'none' }}
+                    />
+            </div>
 
             <div className='row'>
                 <div className='col-6'>
@@ -41,7 +108,19 @@ export const Main = () =>{
                         />
                     </div>
                 </div>
+                
+            </div>
+
+            {/* Topic Modeling Graphs */}
+            <div className="row">
+                <h4>Policy Document Topic Modeling Graphs</h4>
+                <iframe
+                    src=""
+                    title="Plotly Graph"
+                    style={{ width: '100%', height: '400px', border: 'none' }}
+                />
             </div>
         </div>
+
     )
 }
